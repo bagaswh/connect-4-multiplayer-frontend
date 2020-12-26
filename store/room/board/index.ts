b4f22dc1517eed11d22617f12b33152e9743eda1
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2020-12-26 20:05:42 +0700

    feat: move boardGrid state to store

--- store/room/board/index.ts
+++ store/room/board/index.ts
@@ -1,6 +1,6 @@
 import { Module, VuexModule, Mutation } from 'vuex-module-decorators';
-import Board, { BoardConfig } from '~/modules/game/game/Board';
-import { Color } from '~/modules/game/game/Cell';
+import Board, { BoardConfig, Win } from '~/modules/game/game/Board';
+import Cell, { Color } from '~/modules/game/game/Cell';
 
 @Module({
   name: 'room/board',
@@ -10,29 +10,38 @@ import { Color } from '~/modules/game/game/Cell';
 export default class BoardModule extends VuexModule {
   board!: Board;
   initialized = false;
+  boardGrid: Cell[][] = [[]];
+  win: Win | undefined = undefined;
+
+  get boardConfig() {
+    return this.board.config;
+  }
 
   @Mutation
   initBoard(config?: BoardConfig) {
     this.board = new Board(config);
+    this.boardGrid = this.board.getGrid();
     this.initialized = true;
-  }
-
-  @Mutation
-  drop({ x, color }: { x: number; color: Color }) {
-    this.board.drop(x, color);
   }
 
   @Mutation
   dropHint(x: number) {
     this.board.dropHint(x);
+    this.boardGrid = this.board.getGrid();
   }
 
   @Mutation
   removeDropHints() {
     this.board.removeDropHints();
+    this.boardGrid = this.board.getGrid();
   }
 
-  get boardConfig() {
-    return this.board.config;
+  @Mutation
+  drop({ x, color }: { x: number; color: Color }) {
+    const win = this.board.drop(x, color);
+    if (win) {
+      this.win = win;
+    }
+    this.boardGrid = this.board.getGrid();
   }
 }
